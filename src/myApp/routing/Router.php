8a1330ed2b7ba77417cb2be577b1ac184df77d67
cyril auburtin <auburtic@mymed20.sophia.inria.fr>
2012-11-18 14:55:28 +0100

    Revert "clean router again"
    This reverts commit f8aee4934a14d00757ee0957e152c6b70a4b0e72.

--- src/myApp/routing/Router.php
+++ src/myApp/routing/Router.php
@@ -31,7 +31,7 @@ class Router {
 		}
 		
 		// Authentication Access Control
-		if ( !isset($_SESSION['user']) ){
+		if (!isset($_SESSION['user']) && $controllerName !== 'authentication'){
 			$login = new \myApp\controllers\Login;
 			$login->read();
 		}
